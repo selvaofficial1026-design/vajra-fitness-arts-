9ed06c7b4a8e603837d73d0b71e32430d134a926
selvaofficial1026-design <selvaofficial1026@gmail.com>
2026-09-13 08:49:41 +0530

    Configure Next.js image qualities

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.4", "localhost:3000"],
   images: {
+    qualities: [75, 90, 98, 100],
     remotePatterns: [
       {
         protocol: "https",
